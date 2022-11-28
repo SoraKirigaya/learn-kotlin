9a57db1eafbf39b562c4e9a48b1073cc0d5407a5
Matthew Ma <watezbe3@gmail.com>
2022-11-28 17:20:56 +0700

    Part 2: Work with an Android Activity

--- app/src/main/java/com/matthew/bullseye/MainActivity.kt
+++ app/src/main/java/com/matthew/bullseye/MainActivity.kt
@@ -2,11 +2,53 @@ package com.matthew.bullseye
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.SeekBar
+import androidx.appcompat.app.AlertDialog
+import com.matthew.bullseye.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var sliderValue = 0
+    private lateinit var binding: ActivityMainBinding
+
     //Testing pushing from android studio
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.hitMeButton.setOnClickListener {
+            Log.i("Button CLick Event", "You clicked the Hit Me Button")
+            showResult()
+        }
+
+        binding.seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromuser: Boolean) {
+                sliderValue = progress
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+            }
+
+        })
+    }
+
+    private fun showResult() {
+        val dialogTitle = getString(R.string.result_dialog_title)
+        val dialogMessage = getString(R.string.result_dialog_message, sliderValue)
+//        val dialogMessage = "The slider's value is $sliderValue"
+        val builder = AlertDialog.Builder(this)
+
+        builder.setTitle(dialogTitle)
+        builder.setMessage(dialogMessage)
+        builder.setPositiveButton(R.string.result_dialog_button_text) { dialog, _ ->
+            dialog.dismiss()
+        }
+
+        builder.create().show()
     }
 }
